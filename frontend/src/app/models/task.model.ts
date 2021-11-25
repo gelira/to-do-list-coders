fc8a86d80fae43f33faaa3548873f2cd20ec6605
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-25 14:09:24 -0300

    complementando models

--- frontend/src/app/models/task.model.ts
+++ frontend/src/app/models/task.model.ts
@@ -14,3 +14,19 @@ export interface Task {
   created_at: string;
   updated_at: string;
 }
+
+export interface TaskCreate {
+  title: string;
+  description: string;
+  due_date?: string;
+  priority: Priority;
+  done: boolean;
+}
+
+export interface TaskUpdate {
+  title?: string;
+  description?: string;
+  due_date?: string;
+  priority?: Priority;
+  done?: boolean;
+}
