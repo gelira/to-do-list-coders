1246d27b90978d8cea30518c9656ff5870c45277
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-25 13:59:02 -0300

    importando módulo http client

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { MainModule } from './modules/main/main.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MainModule
+    HttpClientModule,
+    MainModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
