d385dd918c8b570923494d369019740588ab4e3a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-25 08:38:50 -0300

    componente home

--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
-
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class MainModule { }
