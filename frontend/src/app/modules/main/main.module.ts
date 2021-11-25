547d32b87fd8a6534b5c768739e945bb39ce78e3
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-25 13:33:07 -0300

    componente header

--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -5,10 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
