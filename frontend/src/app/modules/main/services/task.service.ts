ed1b51c9e20011a7fabc174d342d527c269a8260
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-26 10:05:59 -0300

    usando serviço para armazenar state

--- frontend/src/app/modules/main/services/task.service.ts
+++ frontend/src/app/modules/main/services/task.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Subscription } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
-import { Task, TaskCreate, TaskUpdate } from 'src/app/models/task.model';
+import { Task, TaskCreate, TaskUpdate, taskDefault } from 'src/app/models/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,37 @@ import { Task, TaskCreate, TaskUpdate } from 'src/app/models/task.model';
 export class TaskService {
   private url = `${environment.apiUrl}/tasks`;
 
+  private _tasks = new BehaviorSubject<Task[]>([]);
+  tasks$ = this._tasks.asObservable();
+
+  private _task = new BehaviorSubject<Task>(taskDefault);
+  task$ = this._task.asObservable();
+
   constructor(private http: HttpClient) { }
 
   listTasks() {
-    return this.http.get<Task[]>(this.url);
+    const obs = this.http.get<Task[]>(this.url);
+    const sub: Subscription = obs.subscribe({
+      next: data => this.tasks = data,
+      error: () => this.tasks = [],
+      complete: () => sub.unsubscribe(),
+    });
   }
 
-  retrieveTasks(id: string) {
-    return this.http.get<Task>(`${this.url}/${id}`);
+  retrieveTask(id: string) {
+    const task = this.tasks.find(t => t._id === id);
+
+    if (task) {
+      this.task = task;
+      return;
+    }
+
+    const obs = this.http.get<Task>(`${this.url}/${id}`);
+    const sub: Subscription = obs.subscribe({
+      next: data => this.task = data,
+      error: () => this.task = taskDefault,
+      complete: () => sub.unsubscribe(),
+    });
   }
 
   createTask(task: TaskCreate) {
@@ -25,10 +49,35 @@ export class TaskService {
   }
 
   updateTask(id: string, task: TaskUpdate) {
-    return this.http.patch<Task>(`${this.url}/${id}`, task);
+    return this.http.patch<Task>(`${this.url}/${id}`, task).pipe(
+      map(data => {
+        this.tasks = this.tasks.map(t => t._id === id ? data : t);
+        return data;
+      })
+    );
   }
 
   deleteTask(id: string) {
-    return this.http.delete(`${this.url}/${id}`);
+    const obs = this.http.delete(`${this.url}/${id}`);
+    const sub: Subscription = obs.subscribe({
+      next: () => this.tasks = this.tasks.filter(t => t._id !== id),
+      complete: () => sub.unsubscribe(),
+    });
+  }
+
+  get tasks() {
+    return this._tasks.getValue();
+  }
+
+  private set tasks(value: Task[]) {
+    this._tasks.next(value);
+  }
+
+  get task() {
+    return this._task.getValue();
+  }
+
+  private set task(value: Task) {
+    this._task.next(value);
   }
 }
